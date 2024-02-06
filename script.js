08910c0fd7ab724eea05ffee96fae4a918e0afec
Raghav Kumar Tonk <86098245+raghavtonk@users.noreply.github.com>
2024-02-06 16:59:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,21 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	if(arr.length<2){
+		return 0;
+	}
+	arr.sort(function(a,b){return a-b});
+	let totalcost = 0;
+	while(arr.length >1){
+		let rope1 = arr.shift();
+		let rope2 = arr.shift();
+
+		let cost = rope1 + rope2;
+		totalcost+=cost;
+		arr.push(cost);
+		arr.sort(function(a,b){return a-b})
+	}
+	return totalcost;
 }
 
 module.exports=mincost;
